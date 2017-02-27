1a16c9d707baded7f280f62ee4af0edc51be18c8
dktobeno1 <zeqizai@163.com>
2017-02-27 08:03:43 +0800

    Create my_coroweb.py

---
+++ my_coroweb.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio, os, inspect, logging, functools
+from urllib import parse
+from aiohttp import web
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+ 
+ def post(path):
+    '''
+    Define decorator @post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+ 
+# RequestHandler目的就是从URL函数中分析其需要接收的参数，从request中获取必要的参数，调用URL函数
+class RequestHandler(object):  # 初始化一个请求处理类
+
+    def __init__(self, func):
+        self._func = asyncio.coroutine(func)
+
+    async def __call__(self, request):  # 任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用
+        # 获取函数的参数表
+        required_args = inspect.signature(self._func).parameters
+        logging.info('required args: %s' % required_args)
+
+        # 获取从GET或POST传进来的参数值，如果函数参数表有这参数名就加入
+        kw = {arg: value for arg, value in request.__data__.items() if arg in required_args}
+
+        # 获取match_info的参数值，例如@get('/blog/{id}')之类的参数值
+        kw.update(request.match_info)
+
+        # 如果有request参数的话也加入
+        if 'request' in required_args:
+            kw['request'] = request
+
+        # 检查参数表中有没参数缺失
+        for key, arg in required_args.items():
+            # request参数不能为可变长参数
+            if key == 'request' and arg.kind in (arg.VAR_POSITIONAL, arg.VAR_KEYWORD):
+                return web.HTTPBadRequest(text='request parameter cannot be the var argument.')
+            # 如果参数类型不是变长列表和变长字典，变长参数是可缺省的
+            if arg.kind not in (arg.VAR_POSITIONAL, arg.VAR_KEYWORD):
+                # 如果还是没有默认值，而且还没有传值的话就报错
+                if arg.default == arg.empty and arg.name not in kw:
+                    return web.HTTPBadRequest(text='Missing argument: %s' % arg.name)
+
+        logging.info('call with args: %s' % kw)
+        try:
+            return await self._func(**kw)
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+            
+            
+            
+            
+            
+            
+            
